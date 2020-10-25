f8dd551c339ae94ced900072c4cc8e9245d73696
Tony <tonynguyen0925@gmail.com>
2020-10-24 17:52:50 -0700

    added @Repository for the dao

--- src/main/java/com/comp586/bonfilms/dao/FilmDetailRepository.java
+++ src/main/java/com/comp586/bonfilms/dao/FilmDetailRepository.java
@@ -3,9 +3,11 @@ package com.comp586.bonfilms.dao;
 import com.comp586.bonfilms.models.FilmDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(path = "film-details")
+@Repository
 public interface FilmDetailRepository extends JpaRepository<FilmDetail, Integer> {
 }
--- src/main/java/com/comp586/bonfilms/dao/ReviewRepository.java
+++ src/main/java/com/comp586/bonfilms/dao/ReviewRepository.java
@@ -2,8 +2,10 @@ package com.comp586.bonfilms.dao;
 
 import com.comp586.bonfilms.models.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
 }
