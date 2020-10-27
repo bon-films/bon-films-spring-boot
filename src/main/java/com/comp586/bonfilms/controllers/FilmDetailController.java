b85ea486e3f3495e16b93f0e86addef51085d315
Tony <tonynguyen0925@gmail.com>
2020-10-27 16:40:59 -0700

    added @CrossOrigin to controller to allow connection to angular

--- src/main/java/com/comp586/bonfilms/controllers/FilmDetailController.java
+++ src/main/java/com/comp586/bonfilms/controllers/FilmDetailController.java
@@ -6,6 +6,7 @@ import com.comp586.bonfilms.models.FilmDetail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin("http://localhost:4200")
 @RequestMapping("/api")
 public class FilmDetailController {
     @Autowired
--- src/main/java/com/comp586/bonfilms/controllers/ReviewController.java
+++ src/main/java/com/comp586/bonfilms/controllers/ReviewController.java
@@ -11,6 +11,7 @@ import com.comp586.bonfilms.models.Review;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("http://localhost:4200")
 @RequestMapping("/api")
 public class ReviewController {
 
